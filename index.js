ce87e5198531029252f2892b17e2ba0f457ff9e9
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2018-09-26 11:50:24 +0900

    Delete the first name and reduce the display area

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 const postChatworkMessage = require('post-chatwork-message')
 const getSchedulesFromFusion = require('./lib/get-schedule-from-fusion')
 const template = require('./lib/template')
+const filter = require('./lib/filter')
 const secret = require('./secret.json')
 
 const roomId = process.argv[2] || '18211191'
 const date = new Date()
 const today = `${ date.getFullYear() }/${ date.getMonth() + 1 }/${ date.getDate() }`
 
-  !(async() => {
+  !(async () => {
     const result = await getSchedulesFromFusion(today)
-    const formatted = template(today)(
-      result
-        .filter(s => s.schedules.length)
-        .map(s => [s.name.padEnd(12, '　'), s.schedules])
-    )
+    const formatted = template(today)(filter(result))
 
     console.log(formatted)
     postChatworkMessage(secret.chatworkApiToken, roomId, formatted)
