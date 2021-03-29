2da815c96fb6c86ec268e447275473cd748fe427
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-03-29 14:04:24 +0900

    misc: Update dependencies for the Chromium

--- setup/provisioning.sh
+++ setup/provisioning.sh
@@ -10,8 +10,28 @@ ssh -oStrictHostKeyChecking=no -i $PEM ec2-user@$HOST << EOF
   sudo yum -y install nodejs
 
   # Shared libraries for chronium
-  # See https://github.com/GoogleChrome/puppeteer/issues/391#issuecomment-325420271
-  sudo yum install pango.x86_64 libXcomposite.x86_64 libXcursor.x86_64 libXdamage.x86_64 libXext.x86_64 libXi.x86_64 libXtst.x86_64 cups-libs.x86_64 libXScrnSaver.x86_64 libXrandr.x86_64 GConf2.x86_64 alsa-lib.x86_64 atk.x86_64 gtk3.x86_64 ipa-gothic-fonts xorg-x11-fonts-100dpi xorg-x11-fonts-75dpi xorg-x11-utils xorg-x11-fonts-cyrillic xorg-x11-fonts-Type1 xorg-x11-fonts-misc -y
+  # See https://github.com/puppeteer/puppeteer/issues/391#issuecomment-666541972
+  yum install -y alsa-lib.x86_64 \
+  atk.x86_64 \
+  cups-libs.x86_64 \
+  gtk3.x86_64 \
+  ipa-gothic-fonts \
+  libXcomposite.x86_64 \
+  libXcursor.x86_64 \
+  libXdamage.x86_64 \
+  libXext.x86_64 \
+  libXi.x86_64 \
+  libXrandr.x86_64 \
+  libXScrnSaver.x86_64 \
+  libXtst.x86_64 \
+  pango.x86_64 \
+  xorg-x11-fonts-100dpi \
+  xorg-x11-fonts-75dpi \
+  xorg-x11-fonts-cyrillic \
+  xorg-x11-fonts-misc \
+  xorg-x11-fonts-Type1 \
+  xorg-x11-utils
+  yum update nss -y
 
   # Create application directory
   mkdir birdmen
