3cbcb9ccb7e9a55e9790c44e2f6cf9c07207c620
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-04-08 11:36:16 +0900

    feature: Disable notification to the Chatwork
    BREAKING CHANGE: Do not notify to the Chatwork

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const JapaneseHolidays = require('japanese-holidays')
-const postChatworkMessage = require('post-chatwork-message')
 const getSchedulesFromFusion = require('./lib/get-schedule-from-fusion')
-const template = require('./lib/template')
 const filter = require('./lib/filter')
 const secret = require('./secret.json')
 const request = require("superagent")
 
-const roomId = process.argv[2] || '173667622'
-const slackUrl = process.argv[3] ? secret.slackProductionUrl : secret.slackDevelopmentUrl
+const slackUrl = process.argv[2] ? secret.slackProductionUrl : secret.slackDevelopmentUrl
 
 const date = new Date()
 if(JapaneseHolidays.isHoliday(date)) return
@@ -16,10 +13,7 @@ const today = `${ date.getFullYear() }/${ date.getMonth() + 1 }/${ date.getDate(
 
   !(async () => {
     const result = await getSchedulesFromFusion(today)
-    const formatted = template(today)(filter(result))
-
-    console.log(formatted)
-    postChatworkMessage(secret.chatworkApiToken, roomId, formatted)
+    filter(result)
 
     const formmattedForSlack = {
       blocks: [
